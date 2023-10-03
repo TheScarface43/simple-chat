6f91c24bc750792ccb1244eb39c82b40d884a726
TheScarface43 <barti123.96@gmail.com>
2023-10-03 11:33:49 +0200

    Remove unused MessageType enum constants

--- src/main/java/com/example/simplechat/MessageType.java
+++ src/main/java/com/example/simplechat/MessageType.java
@@ -3,11 +3,9 @@ package com.example.simplechat;
 public enum MessageType {
     SERVER,
     CHAT,
-    WARNING,
     LOCAL,
     COMMAND,
     USERLIST_DATA,
-    USERLIST_ELEMENT,
     NAME_CHANGE,
     ASSIGN_USER
 }
